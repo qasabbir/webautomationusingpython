f0d74843e465c411c3535c4ce7c43a3e5db3ec03
sabbir214586@gmail.com <sabbir214586@gmail.com>
2022-07-27 23:51:41 +0600

    Initial commit

---
+++ automationpackage/automation.py
@@ -0,0 +1,12 @@
+import time
+
+from selenium import  webdriver
+from selenium.webdriver.common.by import By
+
+driver=webdriver.Chrome(executable_path="/Users/codemen/Documents/browserdriver/chromedriver")
+driver.get("https://opensource-demo.orangehrmlive.com/")
+print(driver.title)
+driver.find_element(By.ID,"txtUsername").send_keys("Admin")
+driver.find_element(By.ID,"txtPassword").send_keys("admin123")
+driver.find_element(By.NAME,"Submit").click()
+time.sleep(5)
